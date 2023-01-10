e3fa60a10fe1f57302b8c14a10c41d3561f1be62
Kateryna Pohrebniak <kateryna.pohrebniak@MacBook-Pro-2.local>
2023-01-10 19:44:53 +0100

    actions

--- packages/container/src/components/Marketing.js
+++ packages/container/src/components/Marketing.js
@@ -3,7 +3,7 @@ import { mount } from 'marketing/MarketingApp';
 
 export default () =>{
     const ref = useRef(null);
-    useEffect(()=>{
+    useEffect(() => {
         mount(ref.current);
     },[])
 
