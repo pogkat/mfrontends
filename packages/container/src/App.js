e9799825f5461fb9002a59c1698282e2fd1304c0
Kateryna Pohrebniak <kateryna.pohrebniak@MacBook-Pro-2.local>
2023-01-11 10:33:39 +0100

    header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter }  from "react-router-dom";
 import Marketing from './components/Marketing'
+import Header from './components/Header'
 
 
 export default () => {
     return (
-        <div>
-           <Marketing/>
-        </div>
+        <BrowserRouter>
+            <div>
+                <Header/>
+               <Marketing/>
+            </div>
+        </BrowserRouter>
     );
 };
